878926942d276f968a375506f67c68814797f0df
rpb <bowbione@gmail.com>
2020-01-27 22:06:37 -0800

    moved position

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,19 +6,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
-      },
-
-      resolve: `gatsby-plugin-google-analytics`,
+    resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: "UA-157137903-1",
@@ -34,6 +22,17 @@ module.exports = {
         pageTransitionDelay: 0,
         siteSpeedSampleRate: 10,
         cookieDomain: "rpb.world"
+      },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
