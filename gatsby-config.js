a37bcfe8a4c2a6a585a5af2e4479c0532f9b1633
rpb <bowbione@gmail.com>
2020-01-27 21:50:39 -0800

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,24 @@ module.exports = {
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-157137903-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "rpb.world"
+      },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ],
